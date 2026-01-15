d4909a67c6f742a8cc1dd3b35e75172fc3b67563
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-01-15 14:13:24 +0530

    Initial Commit

--- StringQuestions.js
+++ StringQuestions.js
@@ -2,3 +2,9 @@
 let string = "Java is fun"
 console.log(string.split(' ').reverse().join(' '))
 
+// Check for Rotation Given two strings, check if one is a rotation of another. Input: "ABCD", "CDAB" → Output: true
+
+let s1 = "ABCD"
+let s2 = "ADCB"
+
+console.log((s1.length === s2.length) && (s1+s1).includes(s2))
