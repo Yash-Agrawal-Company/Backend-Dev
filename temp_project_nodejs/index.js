737b9303981fadf9db84be2e01582d9d2268718f
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-01-23 15:29:57 +0530

    Student Crud

---
+++ temp_project_nodejs/index.js
@@ -0,0 +1,6 @@
+import createStudent from "./src/createStudent.js";
+import deleteStudent from "./src/deleteStudent.js";
+import studentSearch from "./src/studentSearch.js";
+// console.log(createStudent('mohan',1234,'bsc'))
+// console.log(studentSearch(1235))
+console.log(deleteStudent(1236))
