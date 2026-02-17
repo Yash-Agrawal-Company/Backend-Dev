da56cd83b618879cafc60505cfa7169d7d880084
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-02-17 17:42:36 +0530

    [Yash Agrawal].add signup added

--- Mini-project/index.js
+++ Mini-project/index.js
@@ -1,14 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { createUser } from './src/model/user.js';
+import { StatusCodes } from 'http-status-pro-js';
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/data', (req, res) => {
+   const {name,email,password} = req.body;
+   if(!name || !email || !password){
+     return res.status(StatusCodes.BAD_REQUEST.code).json({
+        code : StatusCodes.BAD_REQUEST.code,
+        message : StatusCodes.BAD_REQUEST.message,
+        data : null
+     })
+   }    
+   createUser(name,email,password);
+    res.status(StatusCodes.CREATED.code).json({
+        code : StatusCodes.CREATED.code,
+        message : StatusCodes.CREATED.message,
+        data : null
+     })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
---
+++ Mini-project/src/model/user.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+
+export function createUser(name, email,password){
+    let employees = []
+    let objUser = {
+        id : Date.now(), name , email, password
+    }
+    if(fs.existsSync('employees.json')) {
+        let data = JSON.parse(fs.readFileSync('employees.json', 'utf-8'));
+        if(!data){
+            return null;
+        }
+        employees = data;
+    }
+    
+    employees.push(objUser);
+    fs.writeFileSync('employees.json', JSON.stringify(employees, null, 2));
+    console.log("User created successfully")
+}
+
+export function updateUser(id,name,email,password){
+    if(fs.existsSync('employees.json')) {
+        let data = JSON.parse(fs.readFileSync('employees.json', 'utf-8'));
+        if(!data){
+            return null;
+        }
+        let employees = data;
+        employees.map((user) => {
+            if(user.id == id){
+                user.name = name;
+                user.email = email;
+                user.password = password;
+            }
+        })
+        fs.writeFileSync('employees.json', JSON.stringify(employees, null, 2));
+        console.log("User updated successfully")
+    }
+}
+
