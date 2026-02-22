132a75feed409dbb0bbc6dd5dff713f210f8ce34
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-02-22 14:24:30 +0530

    [Yash Agrawal].add fourth question completed

--- express-framework/index.js
+++ express-framework/index.js
@@ -52,6 +52,9 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.use((req, res) => {
+    res.status(404).render("404");
+});
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
