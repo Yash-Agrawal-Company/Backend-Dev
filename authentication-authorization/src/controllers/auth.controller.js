9f4ffdea1267cfe3a1ee4650060b71cb52e4790a
Yash Agrawal <yashagrawal9319029562@gmail.com>
2026-04-13 16:59:33 +0530

    [Yash Agrawal].add schema design added

--- authentication-authorization/src/controllers/auth.controller.js
+++ authentication-authorization/src/controllers/auth.controller.js
@@ -5,19 +5,18 @@ import bcrypt from "bcrypt";
 export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    // 1. check user
+   
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
-    // 2. compare password
+ 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // 3. generate token
     const token = generateToken(user);
 
     res.status(200).json({
